c09b6513598b60212acb8e66650d12d96da39b2e
PruthviUR <129134817+PruthviUR@users.noreply.github.com>
2023-07-28 23:16:58 +0530

    Delete project-1 directory

--- project-1/src/main/java/com/app/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.entities.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Long>{
-
-}
